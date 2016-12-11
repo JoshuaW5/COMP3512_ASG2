8a287e8c9ebe0ecf88135fe202c62f1a2adef500
Coultonf <cjrfraser@gmail.com>
2016-12-10 19:05:22 -0700

    finished animation for loading browse paintings

--- browse-paintings.php
+++ browse-paintings.php
@@ -9,14 +9,14 @@ require 'includes/BrowsePaintingsLogic.php';
 <!DOCTYPE html>
 <html lang=en>
 <head>
-<meta charset=utf-8>
+    <meta charset=utf-8>
     <link href='http://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script src="css/semantic.js"></script>
-	<script src="js/misc.js"></script>
+    <script src="js/misc.js"></script>
     <script src="js/PaintingFunctions.js"></script>
 
     <link href="css/semantic.css" rel="stylesheet" >
@@ -29,63 +29,74 @@ require 'includes/BrowsePaintingsLogic.php';
         <?php include 'includes/header.php';?>
     </header>
 
-<main>
-	<div class="ui segment stackable grid container">
-<div class="five wide column">
-<div class="row">
-<h3 class="filters-header">Filters</h3>
-<div class="ui fitted divider"></div>
-<div class="ui form">
-<form action="browse-paintings.php">
+    <main>
+        <div class="ui segment stackable grid container">
+            <div class="five wide column">
+                <div class="row">
+                    <h3 class="filters-header">Filters</h3>
+                    <div class="ui fitted divider"></div>
+                    <div class="ui form">
+                        <form action="browse-paintings.php">
                             <div class="field">
                                 <label class="filters-label">Artist</label>
                                 <select id="frame" class="ui search dropdown" name="artist">
                                     <option value=''>Select Artist</option>
                                     <?php foreach ($artists as $names) {
-									echo '<option value=' . $names['ArtistID'] . '>' . $names['FirstName'] . ' ' . $names['LastName'] . '</option>'; }?>
+                                        echo '<option value=' . $names['ArtistID'] . '>' . $names['FirstName'] . ' ' . $names['LastName'] . '</option>'; }?>
 
-                                </select>
-                            </div>
-                            <div class="field">
-                                <label>Museum</label>
-                                <select id="frame" class="ui search dropdown" name="museum">
-                                    <option value=''>Select Museum</option>
-                                    <?php foreach ($galleryName as $mNames) {
-									echo '<option value=' . $mNames['GalleryID'] . '>' . $mNames['GalleryName'] . '</option>'; }?>
-                                </select>
-                            </div>
-                            <div class="field">
-                                <label>Select Shape</label>
-                                <select id="frame" class="ui search dropdown" name="shape">
-                                    <option value=''>None</option>
-                                    <?php foreach ($shapes as $sNames) {
-									echo '<option value=' . $sNames['ShapeID'] . '>' . $sNames['ShapeName'] . '</option>'; }?>
-                                </select>
-                            </div>
+                                    </select>
+                                </div>
+                                <div class="field">
+                                    <label>Museum</label>
+                                    <select id="frame" class="ui search dropdown" name="museum">
+                                        <option value=''>Select Museum</option>
+                                        <?php foreach ($galleryName as $mNames) {
+                                            echo '<option value=' . $mNames['GalleryID'] . '>' . $mNames['GalleryName'] . '</option>'; }?>
+                                        </select>
+                                    </div>
+                                    <div class="field">
+                                        <label>Select Shape</label>
+                                        <select id="frame" class="ui search dropdown" name="shape">
+                                            <option value=''>None</option>
+                                            <?php foreach ($shapes as $sNames) {
+                                                echo '<option value=' . $sNames['ShapeID'] . '>' . $sNames['ShapeName'] . '</option>'; }?>
+                                            </select>
+                                        </div>
                                         <!-- <button type="sumbit" value="Submit" class="ui left labeled icon orange button">
-                      <i class="filter icon"></i>
-                      Filter
-                    </button> -->
-</form>
+                                        <i class="filter icon"></i>
+                                        Filter
+                                    </button> -->
+                                </form>
 
-</div>
-</div>
-</div>
-  <div class="eleven wide column">
-   <h1>Paintings</h1>
-	<p>All Paintings [Showing 20]</p>
-    <div id="PaintingList">
-        
-    </div>
-
-
-  </div>
-  </div>
-
-</main>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="eleven wide column">
+                        <h1>Paintings</h1>
+                        <div class="ui segment">
+                            <div class="ui segment">
+                                    <div class="ui inverted dimmer" id="paintLoader">
+                                        <div class="ui massive text loader">Loading</div>
+                                    </div>
+                                    <p>All Paintings [Showing 20]</p>
+                                    <div id="PaintingList">
+                                    </div>
 
 
 
-<?php include 'includes/footer.php';?>
-</body>
-</html>
+                            </div>
+
+                        </div>
+
+
+
+                    </div>
+                </div>
+
+            </main>
+
+
+
+            <?php include 'includes/footer.php';?>
+        </body>
+        </html>
--- js/PaintingFunctions.js
+++ js/PaintingFunctions.js
@@ -2,82 +2,122 @@ $(function(){
 
 
 
-        var urlBase = 'service-painting.php';
-        var params = {artist:'', museum:'', shape:'', name:''};
+    var urlBase = 'service-painting.php';
+    var params = {artist:'', museum:'', shape:'', name:''};
 
-        buildPaintingCard = function(painting){
+    buildPaintingCard = function(painting){
 
-            var card = '<form class="ui form"><div class="ui two column stackable grid container"><div class="four wide column"><a href="single-painting.php?id=' + painting.PaintingID + '"><img src="images/art/works/square-medium/' + painting.ImageFileName + '.jpg" alt="..." class="image" ></a></div><div class="twelve wide column"><h3 class="ui header"><div class="content">' + painting.Title + '<a href="single-artist.php?id=' + painting.ArtistID + '"><div class="sub header">' + (painting.LastName !== null ? painting.LastName:'') + '</a></div></div></h3><p>' + (painting.Description !== null ? painting.Description:'') + '</p><p><input type="hidden" name="ID" value=' + painting.PaintingID + '><input type="hidden" name="image" value="' + painting.ImageFileName + '">$' + painting.MSRP + '</p>' + '<button class="ui icon orange submit button" formaction="includes/addToCart.php?ID=' + painting.PaintingID + '&image=' + painting.ImageFileName + '"><i class="add to cart icon"></i><button class="ui icon orange submit button" formaction="includes/addToFavorites.php?ID=' + painting.PaintingID + '&image=' + painting.ImageFileName + '"><i class="heart icon"></i></button></div></div></div>';
-
-
-            $('#PaintingList').append(card);
-        };
-
-        getPaintingsBy = function(params)
-        {
-            var data = $.ajax({
-                url:urlBase,
-                data:params,
-                dataType:"json"
-            }
-        )
-        .done(function(result){
-            var data = JSON.parse(result);
-            $.each(data, function(i, item){
-
-                buildPaintingCard(data[i]);
-                //console.log(data[i]);
-            });
-        })
-        .fail(function(){
+        var card = '<form class="ui form"><div class="ui two column stackable grid container"><div class="four wide column"><a href="single-painting.php?id=' + painting.PaintingID + '"><img src="images/art/works/square-medium/' + painting.ImageFileName + '.jpg" alt="..." class="image" ></a></div><div class="twelve wide column"><h3 class="ui header"><div class="content">' + painting.Title + '<a href="single-artist.php?id=' + painting.ArtistID + '"><div class="sub header">' + (painting.LastName !== null ? painting.LastName:'') + '</a></div></div></h3><p>' + (painting.Description !== null ? painting.Description:'') + '</p><p><input type="hidden" name="ID" value=' + painting.PaintingID + '><input type="hidden" name="image" value="' + painting.ImageFileName + '">$' + painting.MSRP + '</p>' + '<button class="ui icon orange submit button" formaction="includes/addToCart.php?ID=' + painting.PaintingID + '&image=' + painting.ImageFileName + '"><i class="add to cart icon"></i><button class="ui icon orange submit button" formaction="includes/addToFavorites.php?ID=' + painting.PaintingID + '&image=' + painting.ImageFileName + '"><i class="heart icon"></i></button></div></div></div>';
 
 
+        $('#PaintingList').prepend(card);
+    };
 
-        })
-        .always(function(){
+    getPaintingsBy = function(params)
+    {
+        var data = $.ajax({
+            url:urlBase,
+            data:params,
+            dataType:"json"
+        }
+    )
+    .done(function(result){
+        var data = JSON.parse(result);
+        $.each(data, function(i, item){
 
-
-
+            $('PaintingList').empty();
+            buildPaintingCard(data[i]);
+            //console.log(data[i]);
         });
-    };
+    })
+    .fail(function(){
 
-    //Initial call on page refresh
+
+
+    })
+    .always(function(){
+
+
+
+    });
+};
+
+//Initial call on page refresh
+getPaintingsBy(params);
+
+resetParams = function(){
+    params = {artist:'', museum:'', shape:'', name:''};
+
+};
+//Event handlers for options
+
+$('#frame[name=artist]').on('change', function(){
+
+    resetParams();
+    params.artist = $(this).val();
+
+    var height = $('#PaintingList').height();
+    $('#PaintingList').transition('slide left');
+    $('#paintLoader').addClass('active');
+    $('#PaintingList').parent().css('height', height);
+    //$('PaintingLoader').addClass('active');
+    setTimeout(
+  function(){
+    $('#PaintingList').parent().css('height', 'auto');
+    $('#PaintingList').empty();
     getPaintingsBy(params);
+    $('#paintLoader').removeClass('active');
+    $('#PaintingList').transition('slide left');
+    //$('PaintingLoader').removeClass('active');
+    $("select#frame[name=shape], select#frame[name=museum]").prop('selectedIndex', 0);
+}, 1000);
 
-    resetParams = function(){
-            params = {artist:'', museum:'', shape:'', name:''};
 
-    };
-    //Event handlers for options
 
-    $('#frame[name=artist]').on('change', function(){
+});
 
-        resetParams();
-        params.artist = $(this).val();
-        $('#PaintingList').empty();
-        getPaintingsBy(params);
-        $("select#frame[name=museum], select#frame[name=shape]").prop('selectedIndex', 0);
+$('#frame[name=museum]').on('change', function(){
 
-    });
+    resetParams();
+    params.museum = $(this).val();
+    var height = $('#PaintingList').height();
+    $('#PaintingList').transition('slide left');
+    $('#paintLoader').addClass('active');
+    $('#PaintingList').parent().css('height', height);
+    //$('PaintingLoader').addClass('active');
+    setTimeout(
+  function(){
+    $('#PaintingList').parent().css('height', 'auto');
+    $('#PaintingList').empty();
+    getPaintingsBy(params);
+    $('#paintLoader').removeClass('active');
+    $('#PaintingList').transition('slide left');
+    //$('PaintingLoader').removeClass('active');
+    $("select#frame[name=shape], select#frame[name=artist]").prop('selectedIndex', 0);
+}, 1000);
 
-    $('#frame[name=museum]').on('change', function(){
+});
 
-        resetParams();
-        params.museum = $(this).val();
-        $('#PaintingList').empty();
-        getPaintingsBy(params);
-        $("select#frame[name=shape], select#frame[name=artist]").prop('selectedIndex', 0);
+$('#frame[name=shape]').on('change', function(){
 
-    });
+    resetParams();
+    params.shape = $(this).val();
+    var height = $('#PaintingList').height();
+    $('#PaintingList').transition('slide left');
+    $('#paintLoader').addClass('active');
+    $('#PaintingList').parent().css('height', height);
+    //$('PaintingLoader').addClass('active');
+    setTimeout(
+  function(){
+    $('#PaintingList').parent().css('height', 'auto');
+    $('#PaintingList').empty();
+    getPaintingsBy(params);
+    $('#paintLoader').removeClass('active');
+    $('#PaintingList').transition('slide left');
+    //$('PaintingLoader').removeClass('active');
+    $("select#frame[name=museum], select#frame[name=artist]").prop('selectedIndex', 0);
+}, 1000);
 
-    $('#frame[name=shape]').on('change', function(){
-
-        resetParams();
-        params.shape = $(this).val();
-        $('#PaintingList').empty();
-        getPaintingsBy(params);
-        $("select#frame[name=museum], select#frame[name=artist]").prop('selectedIndex', 0);
-
-    });
+});
 
 });
