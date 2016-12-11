dcce624de220dfeab3a838cedc17339065f86d9e
JoshuaW5 <joshuawojak@gmail.com>
2016-12-10 20:23:13 -0700

    missing file. sorry

---
+++ js/search.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+ $(window).keydown(function(event){
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+  });
+
+results = "";
+  
+$('.ui.search').search({//autocomplete({
+apiSettings: {
+	action:'search',
+	dataType: 'json',
+	url: 'service-painting.php/?search={query}',
+	onSuccess: function (response, element) {
+	response = JSON.parse(response);
+	console.log(response[0]);
+	}
+},
+source: 'url'
+});
+
+});
